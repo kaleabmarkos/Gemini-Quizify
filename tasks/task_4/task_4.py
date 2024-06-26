c5df6b056ee1b9e0346ebe8cd06546d69c59991e
kaleabmarkos <kaleabgessese@gmail.com>
2024-06-26 15:58:46 -0500

    Task-4 Done

--- tasks/task_4/task_4.py
+++ tasks/task_4/task_4.py
@@ -1,13 +1,18 @@
 # embedding_client.py
-
+import os
 from langchain_google_vertexai import VertexAIEmbeddings
+import streamlit as st
+
+
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "../../authentication.json"
 
 class EmbeddingClient:
     """
-    Task: Initialize the EmbeddingClient class to connect to Google Cloud's VertexAI for text embeddings.
+    Initialize the EmbeddingClient class to connect to Google Cloud's VertexAI for text embeddings.
 
     The EmbeddingClient class should be capable of initializing an embedding client with specific configurations
-    for model name, project, and location. Your task is to implement the __init__ method based on the provided
+    for model name, project, and location. Your task is to implement the __init__(model_name=model_name, project=project, loaction=location) method based on the provided
     parameters. This setup will allow the class to utilize Google Cloud's VertexAIEmbeddings for processing text queries.
 
     Steps:
@@ -22,11 +27,11 @@ class EmbeddingClient:
     - project: The Google Cloud project ID where the embedding model is hosted.
     - location: The location of the Google Cloud project, such as 'us-central1'.
 
-    Instructions:
+    TO-DO:
     - Carefully initialize the 'self.client' with VertexAIEmbeddings in the __init__ method using the parameters.
     - Pay attention to how each parameter is used to configure the embedding client.
 
-    Note: The 'embed_query' method has been provided for you. Focus on correctly initializing the class.
+    
     """
     
     def __init__(self, model_name, project, location):
@@ -35,6 +40,9 @@ class EmbeddingClient:
         # https://python.langchain.com/docs/integrations/text_embedding/google_generative_ai
         self.client = VertexAIEmbeddings(
             #### YOUR CODE HERE ####
+            model_name=model_name,
+            project=project,
+            location=location
         )
         
     def embed_query(self, query):
@@ -62,11 +70,19 @@ class EmbeddingClient:
 
 if __name__ == "__main__":
     model_name = "textembedding-gecko@003"
-    project = "YOUR PROJECT ID HERE"
+    project = "quizify-427421"
     location = "us-central1"
 
+    # embedding_client = EmbeddingClient(model_name, project, location)
+    # vectors = embedding_client.embed_query("Hello World!")
+    # if vectors:
+    #     print(vectors)
+    #     print("Successfully used the embedding client!")
     embedding_client = EmbeddingClient(model_name, project, location)
     vectors = embedding_client.embed_query("Hello World!")
+
     if vectors:
-        print(vectors)
-        print("Successfully used the embedding client!")
+        st.write("Successfully used the embedding client!")
+        st.write(vectors)
+    else:
+        st.write("Failed to use the embedding client.")
